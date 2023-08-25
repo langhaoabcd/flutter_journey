a73009b0c5386e26c4220024d5bc6f55ea32c775
胡椒粉 <39892654+langhaoabcd@users.noreply.github.com>
2023-08-25 20:40:19 +0800

    splash

--- myflutter3/README.md
+++ myflutter3/README.md
@@ -5,16 +5,5 @@ A new Flutter project.
 ## Getting Started
 ### 构建和发布为 Android 应用
 - https://flutter.cn/docs/deployment/android
-
-* 添加启动图标
-* 启用 Material 组件
-* 创建一个密钥库
-* 使用 R8 缩小你的代码体积
-* 启用 MultiDex
-* 检查 app manifest 文件
-* 检查构建配置
-* 为发布构建应用
-* 发布到 Google Play Store
-* 更新应用版本号
-* Android 发布常见问题
-
+- 生成图标   flutter pub run flutter_launcher_icons
+- 生成启动页 dart run flutter_native_splash:create
--- myflutter3/lib/main.dart
+++ myflutter3/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
@@ -106,6 +107,26 @@ class _MyHomePageState extends State<MyHomePage> {
     endSessionEndpoint: 'https://oidc-dev.shashago.com/connect/logout',
   );
 
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    // print('ready in 2...');
+    // await Future.delayed(const Duration(seconds: 1));
+    // print('ready in 1...');
+    // await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done

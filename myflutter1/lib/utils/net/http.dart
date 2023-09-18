0100db7762a209dc1f247f948447d8d0fb71d327
胡椒粉 <39892654+langhaoabcd@users.noreply.github.com>
2023-09-18 17:43:55 +0800

    Update http.dart

--- myflutter1/lib/utils/net/http.dart
+++ myflutter1/lib/utils/net/http.dart
@@ -2,90 +2,6 @@ import 'package:dio/dio.dart';
 
 import 'interceptors/timeout_interceptor.dart';
 
-/*
- * 网络管理
- */
-// class Http {
-//   // static Http _instance = Http._internal();
-//   // factory Http() {
-//   //   if (_instance == null) {
-//   //     _instance = Http._internal();
-//   //   }
-//   //   return _instance;
-//   // }
-
-//   // Http _instance;
-
-//   // static Http getInstance() {
-//   //   _instance = Http();
-//   //   return _instance;
-//   // }
-//   // late Dio dio;
-//   // Http._internal() {
-//   //   dio = Dio();
-//   //   dio.options.baseUrl = "http://localhost:5141";
-//   //   dio.options.contentType = "application/json";
-//   //   dio.options.headers = {"Content-Type": "application/json; charset=utf-8"};
-//   // }
-
-//   // Dio dio = Dio();
-
-//   // Http() {
-//   //   // dio.options.baseUrl = "https://www.wanandroid.com";
-//   //   dio.options.baseUrl = "http://localhost:5141";
-//   //   dio.options.contentType = "application/json";
-//   //   dio.options.headers = {"Content-Type": "application/json; charset=utf-8"};
-//   //   // dio.options.connectTimeout = const Duration(seconds: 30);//交给拦截器
-//   //   // dio.options.receiveTimeout = const Duration(seconds: 30);
-//   //   // dio.interceptors.add(LogInterceptor(responseBody: Constants.isDebug));
-//   //   // dio.interceptors.add(TimeoutInterceptors());
-//   //   // dio.interceptors.add(HeaderInterceptors());
-//   //   // dio.interceptors.add(TokenInterceptors());
-//   //   // dio.interceptors.add(LogsInterceptors());
-//   //   // dio.interceptors.add(ErrorInterceptors());
-//   //   // dio.interceptors.add(ResponseInterceptors());
-//   // }
-
-//   static Future<Map<String, dynamic>> get(
-//       String path, Map<String, dynamic> map) async {
-//     var response = await _instance.dio.get(path, queryParameters: map);
-//     return processResponse(response);
-//   }
-
-//   // static Future<Map<String, dynamic>> post(
-//   //     String path, Map<String, dynamic> map) async {
-//   //   var response = await getInstance()!.dio.post(path, queryParameters: map);
-//   //   return processResponse(response);
-//   // }
-
-//   // /*
-//   //   表单形式
-//   //  */
-//   // static Future<Map<String, dynamic>> postForm(
-//   //     String path, Map<String, dynamic> map) async {
-//   //   var response = await getInstance()!.dio.post(path,
-//   //       data: map,
-//   //       options: Options(
-//   //           contentType: "application/x-www-form-urlencoded",
-//   //           headers: {"Content-Type": "application/x-www-form-urlencoded"}));
-//   //   return processResponse(response);
-//   // }
-
-//   static Future<Map<String, dynamic>> processResponse(Response response) async {
-//     if (response.statusCode == 200) {
-//       var data = response.data;
-//       // var code = data["errorCode"] as int;
-//       // var msg = data["errorMsg"] as String;
-//       // if (code == 0) {
-//       //   return data;
-//       // }
-//       return data;
-//       // throw Exception(msg);
-//     }
-//     throw Exception("server error");
-//   }
-// }
-
 class HttpHelper {
   static const String baseUrl = "http://localhost:5141";
 
@@ -130,6 +46,19 @@ class HttpHelper {
         await dio.post(url, data: data, queryParameters: params);
     return _handleResponse<T>(response);
   }
+
+//   // /*
+//   //   表单形式
+//   //  */
+//   // static Future<Map<String, dynamic>> postForm(
+//   //     String path, Map<String, dynamic> map) async {
+//   //   var response = await getInstance()!.dio.post(path,
+//   //       data: map,
+//   //       options: Options(
+//   //           contentType: "application/x-www-form-urlencoded",
+//   //           headers: {"Content-Type": "application/x-www-form-urlencoded"}));
+//   //   return processResponse(response);
+//   // }
 
   static Future<T> put<T>(String url, {data, params}) async {
     final response = await dio.put(url, data: data, queryParameters: params);

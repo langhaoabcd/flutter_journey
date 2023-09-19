72fe46b6bb1b639354a7c17213a81f6c9fe66397
胡椒粉 <39892654+langhaoabcd@users.noreply.github.com>
2023-09-19 14:09:56 +0800

    x

--- myflutter1/lib/services/WeatherService.dart
+++ myflutter1/lib/services/WeatherService.dart
@@ -4,13 +4,16 @@ import '../utils/net/http.dart';
 
 class WeatherService {
   static Future<List<WeatherForecast>> list() async {
-    var res = await HttpHelper.get('/weatherforecast', params: null);
-    List<WeatherForecast> result = [];
-    res.forEach((element) {
-      final map = element.cast<String, dynamic>();
-      var wf = WeatherForecast.fromJson(map);
-      result.add(wf);
-    });
+   var res = await HttpHelper.get('/weatherforecast', params: null);
+   var  result =  res.map<WeatherForecast>((map) => WeatherForecast.fromJson(map)).toList();
+   return result;
+
+   //List<WeatherForecast> result = [];
+   // res.forEach((elem ent) {
+    //   // final map = element.cast<String, dynamic>();
+    //   var wf = WeatherForecast.fromJson(element);
+    //   result.add(wf);
+    // });
     return result;
   }
 }
